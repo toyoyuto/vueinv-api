1aa2dfb379741068548cabdec1992987604e0443
豊岡祐人 <toyooka.yuto@baroque-w.jp>
2020-03-01 10:04:18 +0900

    # mailtest

---
+++ app/Console/Commands/SendTestMail.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
+
+class SendTestMail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mail:send_test_mail';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'テストメールを送信します。';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // とりあえず携帯に送信
+        Mail::to('toyoyuto618@docomo.ne.jp')->send(new TestMail());
+    }
+}
